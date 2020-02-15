e50ebde60bd120d74c30aaecfa5e00b4a2e844ab
Utku Demir <me@utdemir.com>
2020-02-16 11:59:58 +1300

    Remove publish.sh from the derivation

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ in
 pkgs.stdenv.mkDerivation {
   name = "blog";
   buildInputs = with pkgs; [ bash pandoc jq findutils rsync linkchecker ];
-  src = pkgs.nix-gitignore.gitignoreSource [] ./.;
+  src = pkgs.nix-gitignore.gitignoreSource [ "publish.sh" ] ./.;
   phases = "unpackPhase buildPhase";
   buildPhase = ''
     bash ./generate.sh
