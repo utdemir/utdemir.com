fbb849da2ae1e5ab02f0960ac7ae088320e2916e
Utku Demir <me@utdemir.com>
2021-04-12 10:06:53 +1200

    Add netlify-cli

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@ sources = import ./nix/sources.nix;
 pkgs = import sources.nixpkgs {};
 in
 pkgs.mkShell {
-  buildInputs = [ (import ./default.nix).buildInputs pkgs.nodePackages.prettier ];
+  buildInputs = [ (import ./default.nix).buildInputs pkgs.nodePackages.prettier pkgs.nodePackages.netlify-cli ];
   shellHook = ''
     export PYTHONPATH=
   '';
