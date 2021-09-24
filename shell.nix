f443c45141172672e4c4bed89c29d088810fd98d
Utku Demir <me@utdemir.com>
2021-09-24 13:21:43 +1200

    wibbles

--- shell.nix
+++ shell.nix
@@ -3,7 +3,11 @@ sources = import ./nix/sources.nix;
 pkgs = import sources.nixpkgs {};
 in
 pkgs.mkShell {
-  buildInputs = [ (import ./default.nix).buildInputs pkgs.nodePackages.prettier pkgs.nodePackages.netlify-cli ];
+  buildInputs = [
+    (import ./default.nix).buildInputs
+    pkgs.nodePackages.prettier
+    pkgs.nodePackages.netlify-cli
+  ];
   shellHook = ''
     export PYTHONPATH=
   '';
